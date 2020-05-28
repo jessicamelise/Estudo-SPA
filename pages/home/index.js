a3be4ac8f5b0587d335e50864343a778396bd412
jessicamelise <jessica.melise@hotmail.com>
2020-05-28 10:20:27 -0300

    inclusão de texto da pagina home

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,11 @@
 export const home = () => {
   const mainHome = document.createElement("section");
-  const text = `<p>teste sobre meus animes</p>`;
+  const text = `
+    <h1>Sejam bem-vindos!</h1>
+    <h3>Meus animes favoritos</h3>
+    <p>Nessa página listarei alguns dos animes que mais gostei de assistir!</p>
+    <p>Se você não assistiu algum desses, totalmente indico!</p>
+  `;
   mainHome.innerHTML = text;
   return mainHome;
 }
